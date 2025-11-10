40a885244fa3cb2430c0303d117f6854a97c7043
tokec <fleatoke@gmail.com>
2025-11-11 00:22:23 +0900

    Remove all old files

--- drizzle/migrations/0000_vengeful_paper_doll.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `files` (
-	`id` text PRIMARY KEY NOT NULL,
-	`fileName` text NOT NULL,
-	`filePath` text NOT NULL,
-	`contentType` text NOT NULL,
-	`expiresAt` text NOT NULL,
-	`createdAt` text NOT NULL
-);
