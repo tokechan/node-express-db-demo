81f486beee99b0e825d60f86141962a27a6d4326
tokec <fleatoke@gmail.com>
2025-11-11 00:56:12 +0900

    Update package configuration and TypeScript settings for module support

--- node-express-db/src/index.ts
+++ node-express-db/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("server running!!");
+  res.send("server running!!");
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
